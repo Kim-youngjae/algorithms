c78a2008322068dda3467ba673ea67ac15b9e8ef
youngjae459 <youngjae459@gmail.com>
2023-05-21 14:46:23 +0900

    올바른 괄호: if-else 분기문 줄여서 시간초과 문제 해결

--- src/main/java/com/programmers/level2/p12909/Solution.java
+++ src/main/java/com/programmers/level2/p12909/Solution.java
@@ -9,24 +9,17 @@ class Solution {
         boolean answer = true;
 
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ')' && stack.isEmpty()) {
-                return false; // 닫힌 괄호가 남았는데 stack 이 비었으면 올바르지 않은 괄호
+            if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    return false; // 닫힌 괄호가 남았는데 stack 이 비었으면 올바르지 않은 괄호
+                }
+                stack.pop(); // 비어있지 않으면 여는 괄호 하나 꺼내기
             } else if (s.charAt(i) == '(') {
                 stack.push(s.charAt(i)); // 열린 괄호는 스택에 푸시
-            } else if (s.charAt(i) == ')' && !stack.isEmpty()) {
-                // 닫힌 괄호를 만났고 스택이 비어있지 않다면
-                while (!stack.isEmpty()) {
-                    Character data = stack.pop();
-                    if (data == '(') {
-                        break; // 열린 괄호를 만나면 반복문 종료
-                    }
-                }
             }
         }
 
-        if (!stack.isEmpty()) {
-            answer = false;
-        }
+        answer = stack.isEmpty();
 
         return answer;
     }
