7ac8f36f2ebd6a1ca546f2b481c28357cba5c47f
youngjae459 <youngjae459@gmail.com>
2023-05-21 13:27:12 +0900

    flag에 따라 다른 값 반환하기

---
+++ src/main/java/com/programmers/level0/p181933/Solution.java
@@ -0,0 +1,11 @@
+package com.programmers.level0.p181933;
+
+class Solution {
+    public int solution(int a, int b, boolean flag) {
+        if (flag) {
+            return a + b;
+        } else {
+            return a - b;
+        }
+    }
+}
