5e7fd8769f1dd1846b4a379e2b1648e1ecab9700
youngjae459 <youngjae459@gmail.com>
2023-05-22 23:43:10 +0900

    프로세스: 우선 순위 큐를 이용해 해결(공부 필요)

--- src/main/java/com/programmers/level2/p42587/Solution.java
+++ src/main/java/com/programmers/level2/p42587/Solution.java
@@ -1,58 +1,34 @@
 package com.programmers.level2.p42587;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
     public static void main(String[] args) {
         System.out.println(new Solution().solution(new int[]{1, 1, 9, 1, 1, 1}, 0));
     }
 
-    static int cnt = 1;
-    static int maxValue;
-
     public int solution(int[] priorities, int location) {
         int answer = 0;
 
-        Queue<Procedure> q = new LinkedList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // 우선 순위 큐, 내림 차순으로 정렬 기준을 가진다.
 
         for (int i = 0; i < priorities.length; i++) { // 큐에 값 집어넣어 주기
-            q.add(new Procedure(i, priorities[i]));
+            pq.add(priorities[i]);
         }
 
-        Arrays.sort(priorities);
-
-        maxValue = priorities[priorities.length - 1]; // 제일 큰 값 찾아주기
-
-        while (!q.isEmpty()) {
-            // 만약 큐의 중요도가 가장 크다면?
-            Procedure current = q.poll();
-
-            if (current.index == location && current.priority >= maxValue) {
-                answer = cnt++;
-                break;
+        while (!pq.isEmpty()) { // 큐가 비어있지 않은 동안 반복
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == pq.peek()) { // priorities[i] 의 값이 우선 순위 제일 높은 값이랑 같은지 비교
+                    if (i == location) { // 우선 순위를 만족하고 해당 찾고자 하는 인덱스의 값과 일치한다면
+                        answer += 1; // 마지막도 큐에서 나오는 것을 카운트 해줘야 한다.
+                        return answer;
+                    }
+                    pq.poll(); // 알고자 하는 location 인덱스가 아니라면 (값만 같은 경우)
+                    answer += 1; // 큐에서 값을 빼면 카운트
+                }
             }
-
-            if (current.priority < maxValue) {
-                q.add(current); // 다시 큐안에 넣기
-            } else {
-                maxValue = q.peek().priority;
-                cnt++;
-            }
-
         }
 
         return answer;
-    }
-}
-
-class Procedure {
-    int index;
-    int priority;
-
-    public Procedure(int index, int priority) {
-        this.index = index;
-        this.priority = priority;
     }
 }
