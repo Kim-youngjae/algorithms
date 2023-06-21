5d6c50b5c0044261a2eecafe8f2a534034fc0372
youngjae459 <youngjae459@gmail.com>
2023-06-21 09:39:07 +0900

    달리기 경주 성공

--- src/main/java/com/programmers/level1/p178871/Solution.java
+++ src/main/java/com/programmers/level1/p178871/Solution.java
@@ -9,26 +9,35 @@ class Solution {
     public String[] solution(String[] players, String[] callings) {
         answer = new String[players.length];
 
-        HashMap<String, Integer> map = new HashMap<>(players.length);
+        HashMap<String, Integer> nameKeyMap = new HashMap<>(players.length);
+        HashMap<Integer, String> rankKeyMap = new HashMap<>(players.length);
 
         for (int i = 0; i < players.length; i++) {
-            map.put(players[i], i);
-            answer[i] = players[i];
+            nameKeyMap.put(players[i], i);
+            rankKeyMap.put(i, players[i]);
         }
 
         for (String name : callings) {
-            int idx = map.get(name);
-            int changedIdx = swap(idx);
-            map.put(name, changedIdx);
+            // 해설이 부른 선수 이름과 현재 등수
+            int currRank = nameKeyMap.get(name);
+            String currName = rankKeyMap.get(currRank);
+
+            // 추월할 선수
+            String target = rankKeyMap.get(currRank - 1);
+
+            // swap
+            nameKeyMap.put(currName, currRank - 1);
+            nameKeyMap.put(target, currRank);
+
+            rankKeyMap.put(currRank - 1, currName);
+            rankKeyMap.put(currRank, target);
+        }
+
+        // 정답에 넣기
+        for (int i = 0; i < players.length; i++) {
+            answer[i] = rankKeyMap.get(i);
         }
 
         return answer;
-    }
-
-    public static int swap(int n) {
-        String temp = answer[n - 1];
-        answer[n - 1] = answer[n];
-        answer[n] = temp;
-        return n - 1;
     }
 }
