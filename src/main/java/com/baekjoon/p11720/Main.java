090f6e4c6f696902da864d93b8383790a69e6ebd
youngjae459 <youngjae459@gmail.com>
2023-07-08 15:23:26 +0900

    평균 구하기

---
+++ src/main/java/com/baekjoon/p11720/Main.java
@@ -0,0 +1,22 @@
+package com.baekjoon.p11720;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        String sNum = sc.next();
+
+        char[] cNum = sNum.toCharArray();
+
+        int sum = 0;
+
+        for (char c : cNum) {
+            sum += (c - '0');
+        }
+
+        System.out.println(sum);
+    }
+}
