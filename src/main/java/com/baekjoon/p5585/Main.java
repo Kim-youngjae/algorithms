46c1f42aa92e044ce50a22dd9fbbdd7d6881ba02
Kim-youngjae <youngjae459@gmail.com>
2023-10-28 11:22:31 +0900

    거스름 돈 5585

---
+++ src/main/java/com/baekjoon/p5585/Main.java
@@ -0,0 +1,21 @@
+package com.baekjoon.p5585;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int money = 1000 - sc.nextInt();
+        int count = 0;
+
+        int[] changes = new int[] {500, 100, 50, 10, 5, 1};
+
+        for (int i = 0; i < changes.length; i++) {
+            count += money / changes[i];
+            money = money % changes[i];
+        }
+
+        System.out.println(count);
+    }
+}
