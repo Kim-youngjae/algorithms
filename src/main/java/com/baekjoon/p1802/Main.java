8f8d5e50356418d57365d0faeb20c60e6103aa6c
Kim-youngjae <youngjae459@gmail.com>
2023-11-02 10:45:40 +0900

    pass: 종이접기 1802

--- src/main/java/com/baekjoon/p1802/Main.java
+++ src/main/java/com/baekjoon/p1802/Main.java
@@ -1,40 +1,41 @@
 package com.baekjoon.p1802;
 
-import java.io.IOException;
-import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+
 
 public class Main {
-    static String paper;
+    public static String paper;
+    public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
-        int testCase = sc.nextInt();
+        int T = sc.nextInt();
 
-        for (int i = 0; i < testCase; i++) {
+        for (int i = 0; i < T; i++) {
             paper = sc.next();
-
-            if (canFold(0, paper.length() - 1)) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
+            if ( canFold(0, paper.length() - 1)) {
+                sb.append("YES\n");
+            }else {
+                sb.append("NO\n");
             }
         }
+
+        System.out.println(sb);
     }
 
-    static boolean canFold(int start, int end) { // 접을 수 있는지 없는지 체크
-        if (start == end) { // 같으면 중간을 접었을 때 모두 접을 수 있다는 것
-            return true;
-        }
+    private static boolean canFold(int start, int end) {
+        if (start == end) { return true; }
 
-        int middlePoint = (start + end) / 2;
+        int center = (start + end) / 2;
 
-        for (int i = 0; i < middlePoint; i++) {
-            if (paper.charAt(i) == paper.charAt(end) - i) { // 접었을 때에 접을 수 있는지 판단. 같으면 접을 수 없다는 뜻
+        for (int i = start; i < center; i++) {
+            if (paper.charAt(i) == paper.charAt(end - i)) {
                 return false;
             }
         }
 
-        return canFold(start, middlePoint - 1) && canFold(middlePoint + 1, end); // 중간 앞부분과 뒷부분 모두가 참이 나와야 접을 수 있음
+        return canFold(start, center - 1) && canFold(center + 1, end);
     }
 }
