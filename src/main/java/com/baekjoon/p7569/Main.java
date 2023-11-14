327cdcd0c1abd665e258967c5e264f90fb790bb7
Kim-youngjae <youngjae459@gmail.com>
2023-11-14 12:45:15 +0900

    토마토 7569  성공코드

--- src/main/java/com/baekjoon/p7569/Main.java
+++ src/main/java/com/baekjoon/p7569/Main.java
@@ -8,21 +8,20 @@ public class Main {
     static int m, n, h;
     static Queue<Tomato> q1 = new LinkedList<>(); // 1위치를 저장
 
-    static int[] dirRow = { -1, 0, 1, 0, 0, 0 };
-    static int[] dirCol = { 0, 1, 0, -1, 0, 0 };
-    static int[] dirHeight = { 0, 0, 0, 0, 1, -1 };
+    static int[] dirX = { -1, 0, 1, 0, 0, 0 };
+    static int[] dirY = { 0, 1, 0, -1, 0, 0 };
+    static int[] dirZ = { 0, 0, 0, 0, 1, -1 };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        m = Integer.valueOf(st.nextToken());
-        n = Integer.valueOf(st.nextToken());
-        h = Integer.valueOf(st.nextToken());
+        m = Integer.valueOf(st.nextToken()); // 열
+        n = Integer.valueOf(st.nextToken()); // 행
+        h = Integer.valueOf(st.nextToken()); // 면
 
         map = new int[h][n][m];
 
-        // 배열 초기화
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < n; j++) {
                 st = new StringTokenizer(br.readLine(), " ");
@@ -30,90 +29,67 @@ public class Main {
                 for (int k = 0; k < m; k++) {
                     map[i][j][k] = Integer.valueOf(st.nextToken());
                     if (map[i][j][k] == 1) {
-                        q1.add(new Tomato(i, j, k)); // 익은 토마토 위치 정보
+                        q1.add(new Tomato(i, j, k));
+                    }
+
+                }
+            }
+        }
+
+        System.out.println(bfs());
+    }
+
+    private static int bfs() {
+        while (!q1.isEmpty()) {
+            Tomato t = q1.remove();
+
+            int z = t.z; // 면
+            int x = t.x; // 행
+            int y = t.y; // 열
+
+            for (int i = 0; i < 6; i++) {
+                int nx = x + dirX[i];
+                int ny = y + dirY[i];
+                int nz = z + dirZ[i];
+
+                if (nx >= 0 && ny >= 0 && nz >= 0 && nx < n && ny < m && nz < h) {
+                    // 토마토가 익지 않았다면
+                    if (map[nz][nx][ny] == 0) {
+                        // 익지 않은 토마토를 추가
+                        q1.add(new Tomato(nz, nx, ny));
+
+                        map[nz][nx][ny] = map[z][x][y] + 1;
                     }
                 }
             }
         }
 
-        int result = bfs(q1, 0); // 익은 토마토 정보와 날짜를 계산할 변수
-
-        System.out.println(result);
-    }
-
-    private static int bfs(Queue<Tomato> q, int day) {
-
-        while (!q.isEmpty()) {
-            // 큐에서 토마토 위치 정보를 확인
-            Tomato tomato = q.remove();
-
-            int currR = tomato.getX();
-            int currC = tomato.getY();
-            int currH = tomato.getZ();
-
-            for (int i = 0; i < 6; i++) {
-                int nr = currR + dirRow[i];
-                int nc = currC + dirCol[i];
-                int nh = currH + dirHeight[i];
-
-                if (check(nr, nc, nh)) {
-                    q1.add(new Tomato(nr, nc, nh));
-                    map[nh][nr][nc] = map[currH][currR][currC] + 1;
-                }
-            }
-        }
-        // 최대 값을 구하기 위한 변수
         int result = Integer.MIN_VALUE;
 
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < m; k++) {
-                    // 하나라도 익지 않은 토마토가 있다면 -1을 반환
                     if (map[i][j][k] == 0)
                         return -1;
-                    // 토마토가 익는데 걸리는 시간을 구함
                     result = Math.max(result, map[i][j][k]);
                 }
             }
         }
-        // 최대 값이 1이라면 원래부터 모두 익어있었다는 것
+
+        // 토마토가 모두 익은 경우
         if (result == 1)
             return 0;
-        // (최대 값 - 1) --> 걸린 일수
-        else
-            return (result - 1);
-    }
 
-    private static boolean check(int nr, int nc, int nh) {
-        if (nr >= 0 && nr < n && nc >= 0 && nc < m && nh >= 0 && nh < h) {
-            if (map[nh][nr][nc] == 0)
-                return true; // 안익은 토마토 위치
-            else
-                return false; // 이미 익었거나 비어있으면
-        }
-        return false;
-
+        return result - 1;
     }
 }
 
 class Tomato {
-    private int x, y, z;
+    int x, y, z;
 
-    public Tomato(int x, int y, int z) {
+    public Tomato(int z, int x, int y) {
         this.x = x;
         this.y = y;
         this.z = z;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public int getZ() {
-        return z;
     }
 }
