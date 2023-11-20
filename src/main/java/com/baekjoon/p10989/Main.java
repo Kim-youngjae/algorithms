0dc88a1e66d42e7485f9c75c9e8172767dab5b6d
Kim-youngjae <youngjae459@gmail.com>
2023-11-20 11:34:44 +0900

    수 정렬하기 3 10989

---
+++ src/main/java/com/baekjoon/p10989/Main.java
@@ -0,0 +1,37 @@
+package com.baekjoon.p10989;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringBuilder sb = new StringBuilder();
+
+        // int N = Integer.valueOf(br.readLine());
+
+        // int[] numArr = new int[N];
+
+        // for (int i = 0; i < N; i++) {
+        // numArr[i] = Integer.valueOf(br.readLine());
+        // }
+
+        // Arrays.sort(numArr);
+
+        // for (int i : numArr) {
+        // sb.append(i + "\n");
+        // }
+
+        // System.out.println(sb);
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+    }
+}
