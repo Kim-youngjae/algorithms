17b833438f54bd9a9a8c287ce90b8e9f121066d1
Kim-youngjae <youngjae459@gmail.com>
2023-11-21 10:04:32 +0900

    좌표 정렬하기 11650

---
+++ src/main/java/com/baekjoon/p11650/Main.java
@@ -0,0 +1,52 @@
+package com.baekjoon.p11650;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        List<Point> list = new ArrayList<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            list.add(new Point(x, y));
+        }
+
+        Collections.sort(list, (o1, o2) -> {
+            if (o1.x - o2.x > 0) {
+                return 1;
+            } else if (o1.x - o2.x == 0) {
+                return o1.y - o2.y; // 같으면 y 비교
+            } else {
+                return -1;
+            }
+        });
+
+        for (Point p : list) {
+            sb.append(p.x).append(" ").append(p.y).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
+
+class Point {
+
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
