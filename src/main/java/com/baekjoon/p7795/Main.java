021090fe911885649d661d322b641c1d184a2e90
Kim-youngjae <youngjae459@gmail.com>
2023-11-27 11:18:49 +0900

    [7795] 성공코드

--- src/main/java/com/baekjoon/p7795/Main.java
+++ src/main/java/com/baekjoon/p7795/Main.java
@@ -3,50 +3,60 @@ package com.baekjoon.p7795;
 import java.util.*;
 
 public class Main {
+    static int[] A;
+    static int[] B;
+    static int N;
+    static int M;
+    static int result = 0;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
 
         int T = sc.nextInt();
-        int[] A;
-        Map<Integer, Integer> B = new HashMap<>();
 
         for (int t = 1; t <= T; t++) {
-
-            int N = sc.nextInt();
-            int M = sc.nextInt();
+            result = 0;
+            N = sc.nextInt();
+            M = sc.nextInt();
 
             A = new int[N];
-            B.clear();
+            B = new int[M];
 
             for (int i = 0; i < N; i++) {
-                A[i] = sc.nextInt();
+                A[i] = sc.nextInt(); // A 배열 입력
             }
 
             for (int i = 0; i < M; i++) {
-                B.put(sc.nextInt(), 0);
+                B[i] = sc.nextInt(); // B 배열 입력
+            }
+            Arrays.sort(B); // 이분 탐색을 하려고 하는 대상만 정렬해주면 됨
+
+            for (int i = 0; i < N; i++) {
+                binarySearch(A[i]);
             }
 
-            List<Integer> keys = new ArrayList<>(B.keySet());
-
-            for (int i = 0; i < A.length; i++) {
-                for (int j = 0; j < keys.size(); j++) {
-                    int key = keys.get(j);
-                    if (A[i] > key) {
-                        B.replace(key, B.get(key) + 1);
-                    }
-                }
-            }
-
-            int sum = 0; // 순서 쌍 개수
-
-            for (int i : keys) {
-                sum += B.get(i);
-            }
-
-            sb.append(sum).append("\n");
+            sb.append(result + "\n");
         }
         System.out.println(sb);
         sc.close();
+    }
+
+    private static void binarySearch(int target) {
+        int start = 0; // 시작
+        int end = M - 1; // 끝 B의 끝
+        int mid = 0; // 가운데
+
+        while (start <= end) { //
+            mid = (start + end) / 2;
+
+            if (target > B[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        result += start; // start 보다 작으건 모두 먹을 수 있어서 쌍으로 분류
     }
 }
