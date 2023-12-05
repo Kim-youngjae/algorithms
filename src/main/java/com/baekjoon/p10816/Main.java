bd24582a99e7fb4513949e52ce31a7ba4b96490a
Kim-youngjae <youngjae459@gmail.com>
2023-12-05 09:18:46 +0900

    [10816] 숫자카드 2

--- src/main/java/com/baekjoon/p10816/Main.java
+++ src/main/java/com/baekjoon/p10816/Main.java
@@ -1,71 +1,66 @@
 package com.baekjoon.p10816;
 
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    static List<Integer> sortedList;
-    static int M;
-    static Map<Integer, Integer> map = new LinkedHashMap<>();
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int[] array1 = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            array1[i] = sc.nextInt();
-        }
-
-        M = sc.nextInt();
-        int[] array2 = new int[M];
-
-        for (int i = 0; i < M; i++) {
-            int key = sc.nextInt();
-            array2[i] = key;
-            map.put(key, 0);
-        }
-
-        sortedList = new ArrayList<>();
-
-        for (int i : array2) {
-            sortedList.add(i);
-        }
-
-        Collections.sort(sortedList);
-
-        for (int i : array1) {
-            binarySearch(i);
-        }
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        for (int k : sortedList) {
-            sb.append(map.get(k)).append(" ");
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < M; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            sb.append(upperBound(arr, value) - lowerBound(arr, value)).append(" ");
         }
 
         System.out.println(sb);
-        sc.close();
     }
 
-    private static void binarySearch(int target) {
+    private static int lowerBound(int[] arr, int target) {
         int start = 0;
-        int end = M - 1;
-        int mid = 0;
+        int end = arr.length;
 
-        while (start <= end) {
-            mid = (start + end) / 2;
+        while (start < end) {
+            int mid = (start + end) / 2;
 
-            if (target == sortedList.get(mid)) {
-                map.replace(target, map.get(target) + 1);
-                return;
-            }
-            if (target > sortedList.get(mid)) {
-                start = mid + 1;
+            if (target <= arr[mid]) {
+                end = mid;
             } else {
-                end = mid - 1;
+                start = mid + 1;
             }
         }
+
+        return start;
+    }
+
+    private static int upperBound(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length;
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+
+            if (target < arr[mid]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
     }
 }
