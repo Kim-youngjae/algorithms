362bcc453278c266e360b6b3b7232a46b1ee3c09
Kim-youngjae <youngjae459@gmail.com>
2023-12-22 10:19:22 +0900

    BOJ S3 [9095] 1,2,3 더하기

---
+++ src/main/java/com/baekjoon/p9095/Main.java
@@ -0,0 +1,30 @@
+package com.baekjoon.p9095;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[11];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= 10; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+
+            sb.append(dp[N]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
