8ac4daadeb10d1fe20c107a7cf923f9e90dc393b
Kim-youngjae <youngjae459@gmail.com>
2023-12-28 09:36:30 +0900

    BOJ S3 [11726] 2xn 타일링

--- src/main/java/com/baekjoon/p11726/Main.java
+++ src/main/java/com/baekjoon/p11726/Main.java
@@ -1,23 +1,24 @@
 package com.baekjoon.p11726;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        int N = sc.nextInt();
 
-        int[] d = new int[1001];
+        int[] tile = new int[1001];
 
-        d[1] = 1;
-        d[2] = 2;
+        tile[1] = 1;
+        tile[2] = 2;
+        tile[3] = 3;
 
-        for (int i = 3; i <= n; i++) {
-            d[i] = (d[i - 1] + d[i - 2]) % 10007;
+        for (int i = 4; i <= N; i++) {
+            tile[i] = (tile[i - 2] + tile[i - 1]) % 10007;
         }
 
-        System.out.println(d[n]);
+        System.out.println(tile[N]);
         sc.close();
     }
 }
