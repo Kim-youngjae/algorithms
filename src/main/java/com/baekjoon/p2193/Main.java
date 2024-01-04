b5d277bf43e8a524137193bfe7afd414ffaf1173
Kim-youngjae <youngjae459@gmail.com>
2024-01-04 09:41:58 +0900

    BOJ S3 [2193] 이친수

---
+++ src/main/java/com/baekjoon/p2193/Main.java
@@ -0,0 +1,23 @@
+package com.baekjoon.p2193;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        long[] dp = new long[91];
+
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[N]);
+        sc.close();
+    }
+}
