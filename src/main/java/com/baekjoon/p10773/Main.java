f5a3c4b8a0b58f46620d49eea040b28e917d107a
Kim-youngjae <youngjae459@gmail.com>
2024-01-05 10:29:10 +0900

    BOJ S4 [10773] 제로 복습

--- src/main/java/com/baekjoon/p10773/Main.java
+++ src/main/java/com/baekjoon/p10773/Main.java
@@ -1,36 +1,32 @@
 package com.baekjoon.p10773;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
 
-// https://www.acmicpc.net/problem/10773
-// 제로
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Integer> stack = new Stack<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        // 몇 개의 수를 입력 받을지
-        int inputNum = Integer.valueOf(br.readLine());
+        int K = sc.nextInt();
 
-        for (int i = 0; i < inputNum; i++) {
-            int num = Integer.valueOf(br.readLine());
-            if (num == 0) {
-                stack.pop(); // 0 -> 잘 못 부른 수 스택에서 팝
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = 0; i < K; i++) {
+            int val = sc.nextInt();
+
+            if (val == 0) {
+                s.pop();
             } else {
-                stack.push(num); // 0이 아닌 수는 스택에 푸시
+                s.push(val);
             }
         }
 
         int sum = 0;
 
-        while (!stack.isEmpty()) {
-            // stack이 비어있지 않은 동안
-            sum += stack.pop();// 남아있던 모든 수를 뽑아서 저장
+        while (!s.isEmpty()) {
+            sum += s.pop();
         }
 
         System.out.println(sum);
+        sc.close();
     }
 }
