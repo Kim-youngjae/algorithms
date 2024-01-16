02dc22a4c6df00b4fc7ba31835fcc756b69ce479
Kim-youngjae <youngjae459@gmail.com>
2024-01-16 10:42:35 +0900

    BOJ G4 카드 정렬하기

---
+++ src/main/java/com/baekjoon/p1715/Main.java
@@ -0,0 +1,40 @@
+package com.baekjoon.p1715;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> pq = new PriorityQueue<Integer>(Comparator.naturalOrder());
+
+        for (int i = 0; i < N; i++) {
+            pq.offer(Integer.parseInt(br.readLine()));
+        }
+
+        int sum = 0;
+        int total = 0;
+
+        // List<Integer> sumList = new ArrayList<>();
+
+        for (int i = 0; i < N - 1; i++) {
+            int A = pq.remove();
+            int B = pq.remove();
+            sum = A + B;
+            pq.offer(sum);
+            total += sum;
+        }
+
+        // sum = 0;
+
+        // for (int i : sumList) {
+        // sum += i;
+        // }
+
+        System.out.println(total);
+        br.close();
+    }
+}
