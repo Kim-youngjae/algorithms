2ec22581d15fa199bb3650cc06152ffd99d78667
Kim-youngjae <youngjae459@gmail.com>
2024-01-28 23:26:51 +0900

    BOJ DFS와 BFS 2차원 배열 풀이

--- src/main/java/com/baekjoon/p1260/Main.java
+++ src/main/java/com/baekjoon/p1260/Main.java
@@ -3,63 +3,67 @@ package com.baekjoon.p1260;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] connections;
-    static boolean[] visited;
+    static boolean[] visit;
+    static int[][] nodes;
+    static StringBuffer bf = new StringBuffer();
+
+    static int n;
+    static int m;
+    static int v;
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int v = sc.nextInt();
+        n = sc.nextInt(); // 정점의 개수
+        m = sc.nextInt(); // 간선의 개수
+        v = sc.nextInt(); // 탬색을 시작할 정점의 번호
 
-        connections = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
-        for (int i = 1; i <= n; i++) {
-            connections[i] = new ArrayList<Integer>();
-        }
+        visit = new boolean[n];
+        nodes = new int[n][n];
 
         for (int i = 0; i < m; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            connections[x].add(y);
-            connections[y].add(x);
+            int v1 = sc.nextInt();
+            int v2 = sc.nextInt();
+
+            nodes[v1 - 1][v2 - 1] = 1;
+            nodes[v2 - 1][v1 - 1] = 1;
         }
 
-        for (int i = 1; i <= n; i++) {
-            Collections.sort(connections[i]);
-        }
-
-        dfs(v);
-        Arrays.fill(visited, false);
-        bfs(v);
+        dfs(v - 1);
+        bf.append("\n");
+        visit = new boolean[n];
+        bfs(v - 1);
+        System.out.println(bf);
 
         sc.close();
     }
 
-    public static void dfs(int Node) {
-        System.out.print(Node + " ");
-        visited[Node] = true;
-        for (int i : connections[Node]) {
-            if (visited[i] == false) {
+    private static void dfs(int start) {
+        visit[start] = true;
+        bf.append((start + 1) + " ");
+
+        for (int i = 0; i < n; i++) {
+            if (nodes[start][i] == 1 && !visit[i]) {
                 dfs(i);
             }
         }
-        System.out.println();
     }
 
-    public static void bfs(int v) {
-        Queue<Integer> queue = new LinkedList<Integer>();
-        queue.add(v);
-        visited[v] = true;
+    private static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
 
-        while (!queue.isEmpty()) {
-            int Node = queue.poll();
-            System.out.print(Node + " ");
-            for (int i : connections[Node]) {
-                if (visited[i] == false) {
-                    visited[i] = true;
-                    queue.add(i);
+        visit[start] = true;
+        q.add(start);
+
+        while (!q.isEmpty()) {
+            int target = q.remove();
+            bf.append((target + 1) + " ");
+
+            for (int i = 0; i < n; i++) {
+                if (nodes[target][i] == 1 && !visit[i]) {
+                    q.add(i);
+                    visit[i] = true;
                 }
             }
         }
