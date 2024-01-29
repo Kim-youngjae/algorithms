6c16673a4250a67e2dfbc7fa5343b15971f9da37
Kim-youngjae <youngjae459@gmail.com>
2024-01-29 11:53:18 +0900

    BOJ S1 그림

---
+++ src/main/java/com/baekjoon/p1926/Main.java
@@ -0,0 +1,84 @@
+package com.baekjoon.p1926;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[][] nodes;
+    static boolean[][] visit;
+    static int n;
+    static int m;
+    static int[] dr = { 1, 0, -1, 0 };
+    static int[] dc = { 0, 1, 0, -1 };
+    static int painting;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        nodes = new int[n][m];
+        visit = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < m; j++) {
+                nodes[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        painting = 0;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (nodes[i][j] == 1 && !visit[i][j]) {
+                    max = Math.max(max, bfs(i, j));
+                }
+            }
+        }
+
+        System.out.println(painting);
+        System.out.println(max);
+    }
+
+    private static int bfs(int r, int c) {
+        painting++;
+        Queue<Node> q = new LinkedList<>();
+        int size = 0;
+
+        q.add(new Node(r, c));
+        visit[r][c] = true;
+        size++;
+
+        while (!q.isEmpty()) {
+            Node temp = q.poll();
+
+            for (int k = 0; k < 4; k++) {
+                int cr = temp.r + dr[k];
+                int cc = temp.c + dc[k];
+
+                if (cr >= 0 && cr < n && cc >= 0 && cc < m && !visit[cr][cc] && nodes[cr][cc] == 1) {
+                    q.add(new Node(cr, cc));
+                    visit[cr][cc] = true;
+                    size++;
+                }
+            }
+        }
+
+        return size;
+    }
+}
+
+class Node {
+    int r;
+    int c;
+
+    Node(int r, int c) {
+        this.r = r;
+        this.c = c;
+    }
+}
