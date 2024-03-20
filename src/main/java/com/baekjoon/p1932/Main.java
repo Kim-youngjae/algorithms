6fcc94a2286176742476dc728118e3400009c019
Kim-youngjae <youngjae459@gmail.com>
2024-03-20 12:05:55 +0900

    BOJ S1 정수 삼각형 배열 2개 사용한 풀이

--- src/main/java/com/baekjoon/p1932/Main.java
+++ src/main/java/com/baekjoon/p1932/Main.java
@@ -3,36 +3,32 @@ package com.baekjoon.p1932;
 import java.util.*;
 
 public class Main {
+    static int[][] dp;
     static int[][] seq;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        seq = new int[N][N];
+        dp = new int[N + 1][N + 1];
+        seq = new int[N + 1][N + 1];
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j <= i; j++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
                 seq[i][j] = sc.nextInt();
             }
         }
 
-        for (int i = 1; i < N; i++) { // 행
-            for (int j = 0; j <= i; j++) { // 열
-                if (j == 0) {
-                    seq[i][j] += seq[i - 1][j];
-                } else if (j == i) {
-                    seq[i][j] += seq[i - 1][j - 1];
-                } else {
-                    seq[i][j] += Math.max(seq[i - 1][j], seq[i - 1][j - 1]);
-                }
+        for (int i = 1; i <= N; i++) { // 행
+            for (int j = 1; j <= i; j++) { // 열
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1]) + seq[i][j];
             }
         }
 
         int result = Integer.MIN_VALUE;
 
-        for (int i = 0; i < N; i++) {
-            result = Math.max(result, seq[N - 1][i]);
+        for (int i = 1; i <= N; i++) {
+            result = Math.max(result, dp[N][i]);
         }
 
         System.out.println(result);
