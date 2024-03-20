a36c48a91526758795732145375461124a498970
Kim-youngjae <youngjae459@gmail.com>
2024-03-20 11:55:26 +0900

    BOJ S1 정수 삼각형

--- src/main/java/com/baekjoon/p1932/Main.java
+++ src/main/java/com/baekjoon/p1932/Main.java
@@ -1,38 +1,41 @@
 package com.baekjoon.p1932;
 
-import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[][] seq;
 
-        int T = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int[][] dp = new int[T + 1][T + 1];
-        int[][] arr = new int[T + 1][T + 1];
+        int N = sc.nextInt();
+        seq = new int[N][N];
 
-        for (int t = 1; t <= T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int i = 1; i <= t; i++) {
-                arr[t][i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= i; j++) {
+                seq[i][j] = sc.nextInt();
             }
         }
 
-        for (int i = 1; i <= T; i++) {
-            for (int j = 1; j <= T; j++) {
-                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
+        for (int i = 1; i < N; i++) { // 행
+            for (int j = 0; j <= i; j++) { // 열
+                if (j == 0) {
+                    seq[i][j] += seq[i - 1][j];
+                } else if (j == i) {
+                    seq[i][j] += seq[i - 1][j - 1];
+                } else {
+                    seq[i][j] += Math.max(seq[i - 1][j], seq[i - 1][j - 1]);
+                }
             }
         }
 
-        int result = 0;
+        int result = Integer.MIN_VALUE;
 
-        for (int i = 1; i <= T; i++) {
-            if (result < dp[T][i]) {
-                result = dp[T][i];
-            }
+        for (int i = 0; i < N; i++) {
+            result = Math.max(result, seq[N - 1][i]);
         }
 
         System.out.println(result);
+        sc.close();
     }
 }
