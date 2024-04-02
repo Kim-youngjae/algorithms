1557522d9e45536f9367ae0d8fd51a698ffe1ef0
Kim-youngjae <youngjae459@gmail.com>
2024-04-02 11:26:27 +0900

    BOJ G5 평범한 배낭 성공 코드
    짐을 쪼갤 수 없는 문제이므로 dp를 사용한 bottom-up 풀이

--- src/main/java/com/baekjoon/p12865/Main.java
+++ src/main/java/com/baekjoon/p12865/Main.java
@@ -4,8 +4,10 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, K, W, V;
-    static int[] dp;
+    static int N, K;
+    static int[][] dp;
+    static int[] W;
+    static int[] V;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,31 +16,38 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        dp = new int[K + 1];
+        dp = new int[N + 1][K + 1];
+        W = new int[N + 1];
+        V = new int[N + 1];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
 
-            W = Integer.parseInt(st.nextToken());
-            V = Integer.parseInt(st.nextToken());
-
-            dp[W] = V;
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int n = 3; n <= K; n++) {
-            int max = dp[n];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
 
-            for (int i = 1; i <= (n / 2); i++) {
-                dp[n] = Math.max(max, dp[i] + dp[n - i]);
+                /*
+                 * 넣으려고 하는 물건의 무게가 총 무게보다 많이 나가면
+                 * 이전 물건을 수용할 수 있는 최대 무게에 대한 가치의 합이 최대 가치의 합
+                 */
+                if (W[i] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    /*
+                     * 비교하려는 무게가 최대 무게보다 가벼워서 가방에 넣을 수 있는 경우
+                     * (수용하려는 무게 - 넣으려는 물건의 무게에 대한 가치 + 넣으려는 무게의 가치) vs (이전까지 넣은 무게에 대한 가치) 중 더 큰 값이
+                     * 최대 가치의 합
+                     */
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
+                }
             }
         }
 
-        int max = Integer.MIN_VALUE;
-        for (int i = 1; i <= K; i++) {
-            max = Math.max(max, dp[i]);
-        }
-
-        System.out.println(max);
+        System.out.println(dp[N][K]);
 
     }
 }
