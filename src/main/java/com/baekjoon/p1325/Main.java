c293e44d3dcfacd916d2adab2e214d10cde77939
Kim-youngjae <youngjae459@gmail.com>
2024-04-25 12:11:34 +0900

    BOJ S1 효율적인 해킹 bfs 풀이

--- src/main/java/com/baekjoon/p1325/Main.java
+++ src/main/java/com/baekjoon/p1325/Main.java
@@ -1,61 +1,74 @@
 package com.baekjoon.p1325;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, max, cnt;
-    static int[] com;
-    static List<List<Integer>> rel = new ArrayList<>();
-    static boolean[] v;
+    static int N, M;
+    static ArrayList<Integer>[] arr;
+    static boolean v[];
+    static int max;
+    static int cntArr[];
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        com = new int[N + 1];
-        for (int i = 0; i <= N; i++) { // 배열 초기화 1~N
-            rel.add(new ArrayList<Integer>());
-        }
+        cntArr = new int[N + 1];
 
-        for (int i = 0; i < M; i++) { // 신뢰관계 입력받기
-            st = new StringTokenizer(br.readLine());
+        // 신뢰 관계 입력
+        arr = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++)
+            arr[i] = new ArrayList<Integer>();
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(bf.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            rel.get(a).add(b);
+            arr[a].add(b); // a가 b를 신뢰, a는 b에게 해킹 당할 수 있음
         }
 
+        // 1번부터 N번까지 search
         for (int i = 1; i <= N; i++) {
             v = new boolean[N + 1];
-            dfs(i);
+            BFS(i);
         }
 
-        for (int cnt : com) {
-            max = Math.max(cnt, max);
-        }
-
-        StringBuilder sb = new StringBuilder();
+        // 해킹할 수 있는 최댓값 찾기
         for (int i = 1; i <= N; i++) {
-            if (com[i] == max) {
-                sb.append(i + " ");
-            }
+            if (max < cntArr[i])
+                max = cntArr[i];
         }
 
-        System.out.println(sb);
+        // 최댓값인 컴퓨터 출력
+        for (int i = 1; i <= N; i++)
+            if (max == cntArr[i])
+                System.out.print(i + " ");
     }
 
-    static void dfs(int idx) {
-        v[idx] = true;
-        for (int i : rel.get(idx)) {
-            if (!v[i]) {
-                com[i]++;
-                dfs(i);
+    static void BFS(int start) {
+        Queue<Integer> que = new ArrayDeque<Integer>();
+
+        que.add(start);
+        v[start] = true;
+
+        while (!que.isEmpty()) {
+            int now = que.poll();
+            for (int i : arr[now]) {
+                if (!v[i]) {
+                    cntArr[i]++; // i가 해킹할 수 있는 숫자 증가
+                    v[i] = true;
+                    que.add(i);
+                }
             }
         }
-
     }
 }
