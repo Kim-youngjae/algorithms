be6f6dbc2721f91eaf6a5ec09a1773119de36e7d
Kim-youngjae <youngjae459@gmail.com>
2024-05-13 10:09:04 +0900

    BOJ S3 01타일 복습

--- src/main/java/com/baekjoon/p1904/Main.java
+++ src/main/java/com/baekjoon/p1904/Main.java
@@ -3,20 +3,22 @@ package com.baekjoon.p1904;
 import java.io.*;
 
 public class Main {
+    static Integer[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[1000001];
+        arr = new Integer[1000001];
 
-        dp[1] = 1;
-        dp[2] = 2;
+        arr[1] = 1;
+        arr[2] = 2;
 
         for (int i = 3; i <= N; i++) {
-            dp[i] = (dp[i - 2] % 15746 + dp[i - 1] % 15746) % 15746;
+            arr[i] = (arr[i - 1] % 15746 + arr[i - 2] % 15746) % 15746;
         }
 
-        System.out.println(dp[N]);
+        System.out.println(arr[N]);
     }
 }
