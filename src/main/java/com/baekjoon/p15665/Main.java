8c5e1d8e3128ab4f398463c88300e0b90a3e085c
Kim-youngjae <youngjae459@gmail.com>
2024-05-17 11:50:09 +0900

    BOJ S2 N과 M (11) 코드 수정

--- src/main/java/com/baekjoon/p15665/Main.java
+++ src/main/java/com/baekjoon/p15665/Main.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class Main {
     static int N, M;
     static int[] arr, res;
-    static boolean[] v;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) {
@@ -16,7 +15,6 @@ public class Main {
 
         arr = new int[N];
         res = new int[M];
-        v = new boolean[N];
 
         for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
@@ -40,12 +38,11 @@ public class Main {
         }
 
         int before = -1;
-
         for (int i = 0; i < N; i++) {
             int now = arr[i];
             if (before == now) {
                 continue;
-            } else {
+            } else { // 중복되는 수열을 허용해야하기 때문에 방문 체크를 빼주어야 했다.
                 before = now;
                 res[depth] = arr[i];
                 dfs(depth + 1);
