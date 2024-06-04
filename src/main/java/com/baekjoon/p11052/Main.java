99b42205a5bb52b9cc7bf1d6b733c2e8de9ad14b
Kim-youngjae <youngjae459@gmail.com>
2024-06-04 12:03:45 +0900

    BOJ S1 카드 구매하기

---
+++ src/main/java/com/baekjoon/p11052/Main.java
@@ -0,0 +1,27 @@
+package com.baekjoon.p11052;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1], P = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + P[j]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
