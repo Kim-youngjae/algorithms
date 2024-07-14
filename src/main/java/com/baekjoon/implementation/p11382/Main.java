e251579887454406f8b924f970496b09d50a527c
Kim-youngjae <youngjae459@gmail.com>
2024-07-15 07:35:23 +0900

    BOJ 꼬마 정민

---
+++ src/main/java/com/baekjoon/implementation/p11382/Main.java
@@ -0,0 +1,16 @@
+package com.baekjoon.implementation.p11382;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long c = sc.nextLong();
+        long b = sc.nextLong();
+
+        System.out.println((a + b + c));
+        sc.close();
+    }
+}
