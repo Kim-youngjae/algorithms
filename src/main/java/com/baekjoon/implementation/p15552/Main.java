31be4014ca4a9229829362ec00c3b635ec3afa41
Kim-youngjae <youngjae459@gmail.com>
2024-07-15 07:29:31 +0900

    BOJ 빠른 A+B

---
+++ src/main/java/com/baekjoon/implementation/p15552/Main.java
@@ -0,0 +1,24 @@
+package com.baekjoon.implementation.p15552;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write((a + b) + "\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
