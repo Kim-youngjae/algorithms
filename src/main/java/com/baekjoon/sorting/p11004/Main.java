63800b49af4c380c43d5ed10987abbdbd1907ce8
Kim-youngjae <youngjae459@gmail.com>
2024-07-16 11:38:28 +0900

    BOJ S5 K번째 수

---
+++ src/main/java/com/baekjoon/sorting/p11004/Main.java
@@ -0,0 +1,24 @@
+package com.baekjoon.sorting.p11004;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        System.out.println(nums[K - 1]);
+    }
+}
