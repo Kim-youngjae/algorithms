c508396592c395bac409cbee52cfa070c18a0edb
Kim-youngjae <youngjae459@gmail.com>
2024-08-07 19:21:42 +0900

    BOJ B5 등록

---
+++ src/main/java/com/baekjoon/implementation/p7287/Main.java
@@ -0,0 +1,8 @@
+package com.baekjoon.implementation.p7287;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("369");
+        System.out.println("youngjae459");
+    }
+}
