ec519193539d04062f352b32ad66cacdc779d17d
Kim-youngjae <youngjae459@gmail.com>
2024-08-07 18:44:55 +0900

    BOJ B5 단어 길이 재기

---
+++ src/main/java/com/baekjoon/implementation/p2743/Main.java
@@ -0,0 +1,12 @@
+package com.baekjoon.implementation.p2743;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(input.length());
+        sc.close();
+    }
+}
