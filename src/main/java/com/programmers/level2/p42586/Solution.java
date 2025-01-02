33c9cda2c3e3d05add1cc50a1b23448d698926f9
Kim-youngjae <youngjae459@gmail.com>
2025-01-03 06:28:05 +0900

    프로그래머스- 기능개발

--- src/main/java/com/programmers/level2/p42586/Solution.java
+++ src/main/java/com/programmers/level2/p42586/Solution.java
@@ -1,40 +1,29 @@
 package com.programmers.level2.p42586;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
 
-        Queue<Integer> q = new LinkedList<>();
+        int pre = (int) Math.ceil((100.0 - progresses[0]) / speeds[0]);
+        int cnt = 1;
 
-        List<Integer> result = new ArrayList<>(); // 정답을 저장할 리스트
+        List<Integer> result = new ArrayList<>();
 
-        for (int i = 0; i < progresses.length; i++) {
-            int progress = progresses[i];
-            int speed = speeds[i];
-
-            q.add((int) Math.ceil((100.0 - progress) / speed));
-        }
-
-
-        while (!q.isEmpty()) {
-            int ahead = q.poll();
-            int cnt = 1;
-
-            while (!q.isEmpty() && ahead >= q.peek()) {
-                q.poll();
+        for (int i = 1; i < progresses.length; i++) {
+            if (pre >= (int) Math.ceil((100.0 - progresses[i]) / speeds[i])) {
                 cnt++;
+            } else {
+                result.add(cnt);
+                pre = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+                cnt = 1;
             }
-
-            result.add(cnt);
         }
+
+        result.add(cnt);
 
         answer = new int[result.size()];
-
         for (int i = 0; i < result.size(); i++) {
             answer[i] = result.get(i);
         }
